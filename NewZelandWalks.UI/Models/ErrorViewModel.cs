ca21f206a632959894a7de35d164b8b9e8ad7b07
shahazan shahid <shahazan.shahid@gmail.com>
2024-07-21 09:08:05 +0800

    Add project files.

---
+++ NewZelandWalks.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewZelandWalks.UI.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
