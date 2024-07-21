75e08cfd2fcbd67c812518e4d828a059e98c097b
shahazan shahid <shahazan.shahid@gmail.com>
2024-07-21 10:19:44 +0800

    add view model controller dto UI

---
+++ NewZelandWalks.UI/Models/DTO.cs
@@ -0,0 +1,13 @@
+﻿namespace NewZelandWalks.UI.Models
+{
+	public class RegionDto
+	{
+		public Guid Id { get; set; }
+
+		public string Code { get; set; }
+
+		public string Name { get; set; }
+
+		public string? RegionImageUrl { get; set; }
+	}
+}
